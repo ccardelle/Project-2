1099633e7f0fd9a11007affbfd15702af7a90056
Chris Cardelle <chris.cardelle@gmail.com>
2019-05-08 11:38:08 -0400

    Deployment

--- server.js
+++ server.js
@@ -4,7 +4,7 @@
 // get all the tools we need
 var express  = require('express');
 var app      = express();
-var port     = process.env.PORT || 8080;
+// var port     = process.env.PORT || 8080;
 var mongoose = require('mongoose');
 var passport = require('passport');
 var flash    = require('connect-flash');
@@ -61,5 +61,9 @@ hbs.registerHelper('json', function(context) {
 require('./app/routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
 require('./app/billsroute.js')(app, passport)
 // launch ======================================================================
-app.listen(port);
-console.log('The magic happens on port ' + port);
+// app.listen(port);
+// console.log('The magic happens on port ' + port);
+
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  });
