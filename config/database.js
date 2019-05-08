b5741e9744631c336157dd64dcb7c3a5d87f504b
Chris Cardelle <chris.cardelle@gmail.com>
2019-05-08 12:45:23 -0400

    Testing mLab Add-on

--- config/database.js
+++ config/database.js
@@ -1,6 +1,10 @@
 // config/database.js
 module.exports = {
 
-    'url' : 'mongodb://localhost/passport' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+    'url' : 'mongodb://heroku_31jj84c7:onaj9aai33s7op4v9k8e899e5p@ds153566.mlab.com:53566/heroku_31jj84c7'
+    
+    // 'url' : 'mongodb://localhost/passport' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
 
-};
+};  
+
+
